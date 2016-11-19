ce4d8059ebd46938ca5b19407979faa95320ceaf
Kevin Brandon <kevinabrandon@gmail.com>
2016-11-18 23:41:14 -0800

    added comments

--- run_tracker.sh
+++ run_tracker.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+#
+# This is just a simple script that loads the correct python enviroment
+# and then loops forever restarting the tracker program if it ever exits.
+#
 
 cd /home/pi/AboveTustin
 source py3/bin/activate
